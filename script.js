e29fd987d6e0f5e673892f4ad3eed417669312a3
Colin Bell <cobell206@gmail.com>
2021-02-02 20:28:12 -0700

    updated js file

---
+++ script.js
@@ -0,0 +1,13 @@
+// Empty variables for user input
+var song_title = ""
+var artist_name = ""
+
+// Button to pull song title
+$("#search-input").on("click", function() {
+    song_title = $("#search-input").value
+})
+
+// Button to pull song artist
+$("#search-input").on("click", function() {
+    song_title = $("#search-input").value
+})
