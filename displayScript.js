3cd615f320e611e79e5108ae5e620ade1d7e7c1d
Rikio Campbell <rikiocamp@gmail.com>
2021-02-04 20:34:13 -0800

    split lyrics by line, split landingpage into 2 columns, added youtube.js into index

--- displayScript.js
+++ displayScript.js
@@ -1,7 +1,7 @@
 
 // Populate iframe
-static_youtube_url = "https://www.youtube.com/embed/"
-url = ""
+// static_youtube_url = "https://www.youtube.com/embed/"
+// url = ""
 // url = object.items[0].id.VideoId
 // $('#video_player').attr('src', static_youtube_url + url)
 
@@ -9,4 +9,21 @@ url = ""
 var searchLyrics = JSON.parse(localStorage.getItem("projectLyrics"))
 
 // Populate lyrics
-$("#lyrics_box").text(searchLyrics)
+
+var searchLyricsSplit = searchLyrics.split("\n");
+console.log(searchLyricsSplit);
+for (i=0;i<searchLyricsSplit.length;i++){
+    var lyricsRowDiv = $("<div>")
+    var lyricsRowP = $("<p>")
+    lyricsRowDiv.addClass("row")
+    lyricsRowDiv.attr("id","lyricsRowDiv")
+    lyricsRowP.attr("id","lyricsRowP")
+    $("#lyrics_box").append(lyricsRowDiv)
+    $("#lyricsRowDiv").append(lyricsRowP)
+    $("#lyricsRowP").text(searchLyricsSplit[i])
+    lyricsRowDiv.removeAttr("id")
+    lyricsRowP.removeAttr("id")
+}
+
+
+console.log(searchLyricsSplit[0]);
